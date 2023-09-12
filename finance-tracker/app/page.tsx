30d7a9152eeb3b8fb22895a0b52639bd7856c4ff
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-09-12 13:41:50 +0300

    Initial commit
    - Added shadcn

---
+++ finance-tracker/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>Home Page</p>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default HomePage;
