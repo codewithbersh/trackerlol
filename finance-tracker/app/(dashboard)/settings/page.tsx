7f23bcf43145236e89415ab0e36eb200c9d410c8
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-10-07 07:12:44 +0300

    Created Settings Page

---
+++ finance-tracker/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeading } from "@/components/page-heading";
+
+const SettingsPage = () => {
+  return (
+    <div className="mt-[60px] flex  flex-col py-8 pt-0 sm:mt-16 lg:mt-4">
+      <PageHeading title="Settings" />
+    </div>
+  );
+};
+
+export default SettingsPage;
