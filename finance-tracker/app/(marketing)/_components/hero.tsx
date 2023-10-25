d1cb57ce9934ba90e5826fe66eaccfc4d8928a0d
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2023-10-25 13:37:06 +0300

    Updated hero section

--- finance-tracker/app/(marketing)/_components/hero.tsx
+++ finance-tracker/app/(marketing)/_components/hero.tsx
@@ -1,9 +1,19 @@
-import { ArrowRight } from "lucide-react";
+import {
+  ArrowRight,
+  CalendarIcon,
+  ChevronDown,
+  LayoutGrid,
+  Shapes,
+  X,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { COLORS, EMOJIS } from "./config";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { FeaturesHeading } from "./features-heading";
 
 interface HeroProps {
   isAuthenticated: boolean;
@@ -13,25 +23,28 @@ export const Hero = ({ isAuthenticated }: HeroProps) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4 pt-8 sm:pt-0">
       <div className="space-y-1">
-        <div className="animate-fade-up mx-auto w-fit rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-2">
-          <div className="rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-2">
-            <div className="rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-4">
-              <Logo className="h-8 w-8  md:h-12 md:w-12" />
+        <div className="relative mx-auto w-fit">
+          <div className="animate-fade-up relative z-50 mx-auto w-fit rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-2">
+            <div className="rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-2">
+              <div className="rounded-full border-t bg-gradient-to-t from-background to-primary-foreground/50 p-4">
+                <Logo className="h-8 w-8  md:h-12 md:w-12" />
+              </div>
             </div>
           </div>
+          <div className="absolute left-1/2 top-1/2 z-0 h-[150px] w-[150px] -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-full bg-brand/25 blur-2xl backdrop-blur-3xl" />
         </div>
 
         <div className="space-y-4">
           <h1
             className=" animate-fade-up text-center text-4xl font-medium leading-[1.2] tracking-wide text-primary/95 opacity-0 md:text-6xl"
-            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
           >
             The Personal Finance <br className="hidden min-[374px]:block" /> for
             the Web
           </h1>
           <p
             className="animate-fade-up mx-auto max-w-[467px] bg-gradient-to-r from-pink-400 via-indigo-400 to-indigo-600 bg-clip-text text-center font-medium text-transparent opacity-0 md:max-w-[550px] md:text-lg"
-            style={{ animationDelay: "0.50s", animationFillMode: "forwards" }}
+            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
           >
             A simplified finance tracker built for everyone. Navigate your
             finances by tracking your spending and budgets.
@@ -43,7 +56,7 @@ export const Hero = ({ isAuthenticated }: HeroProps) => {
         <Button
           className="animate-fade-up opacity-0"
           variant="ghost"
-          style={{ animationDelay: "0.75s", animationFillMode: "forwards" }}
+          style={{ animationDelay: "0.35s", animationFillMode: "forwards" }}
         >
           Learn More
         </Button>
@@ -51,7 +64,7 @@ export const Hero = ({ isAuthenticated }: HeroProps) => {
           className="animate-fade-up opacity-0"
           variant="brand"
           asChild
-          style={{ animationDelay: "0.85s", animationFillMode: "forwards" }}
+          style={{ animationDelay: "0.45s", animationFillMode: "forwards" }}
         >
           {isAuthenticated ? (
             <Link href="/transactions">
@@ -67,29 +80,141 @@ export const Hero = ({ isAuthenticated }: HeroProps) => {
         </Button>
       </div>
 
-      <div
-        className="animate-fade-up mt-8 rounded-[32px] border p-4 opacity-0"
-        style={{ animationDelay: "1s", animationFillMode: "forwards" }}
-      >
-        <div className=" rounded-[16px] border-[8px] ">
-          <Image
-            src="/transactions-light.jpg"
-            alt="transactions"
-            className="rounded-md  dark:hidden"
-            width={4080}
-            height={2492}
-            priority
-            sizes="100vw"
+      <div className="mt-6 grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div
+          style={{ animationDelay: "0.55s", animationFillMode: "forwards" }}
+          className=" animate-fade-up group col-span-1 rounded-sm bg-accent/25 pb-4 opacity-0 dark:border-t md:pb-8"
+        >
+          <div className="p-4 md:p-8">
+            <FeaturesHeading
+              title="Create Categories"
+              description="Lorem ipsum dolor sit aemt consectur lorem ipsum dolor silasd ndsamet consect."
+            />
+          </div>
+
+          <div className="space-y-4 overflow-hidden">
+            <div className="flex -translate-x-[20px] gap-4">
+              {new Array(12).fill(null).map((_, idx) => (
+                <div
+                  key={idx}
+                  className="grid h-10 w-10 shrink-0 place-items-center rounded-md"
+                  style={{
+                    backgroundColor:
+                      COLORS[Math.floor(Math.random() * COLORS.length)],
+                  }}
+                >
+                  <span className="text-xl leading-none">
+                    {EMOJIS[Math.floor(Math.random() * EMOJIS.length)]}
+                  </span>
+                </div>
+              ))}
+            </div>
+
+            <div className="flex translate-x-2 gap-4">
+              {new Array(12).fill(null).map((_, idx) => (
+                <div
+                  key={idx}
+                  className="grid h-10 w-10 shrink-0 place-items-center rounded-md"
+                  style={{
+                    backgroundColor:
+                      COLORS[Math.floor(Math.random() * COLORS.length)],
+                  }}
+                >
+                  <span className="text-xl leading-none">
+                    {EMOJIS[Math.floor(Math.random() * EMOJIS.length)]}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{ animationDelay: "0.65s", animationFillMode: "forwards" }}
+          className="animate-fade-up col-span-1 row-span-2 aspect-square h-full max-h-[574px] w-full overflow-hidden rounded-sm bg-accent/25 p-4 opacity-0  dark:border-t md:aspect-auto md:p-8"
+        >
+          <FeaturesHeading
+            title="Track Anytime, Anywhere"
+            description="Lorem ipsum dolor sit aemt consectur lorem ipsum dolor silasd ndsamet consect."
           />
-          <Image
-            src="/transactions-dark.jpg"
-            alt="transactions"
-            className="hidden rounded-md dark:block"
-            width={4080}
-            height={2492}
-            priority
-            sizes="100vw"
+
+          <div className=" ">
+            <Image
+              alt=""
+              src="/budgets-mockup-light.png"
+              width={2018}
+              height={4090}
+              className="translate-y-12 dark:hidden"
+            />
+            <Image
+              alt=""
+              src="/budgets-mockup-dark.png"
+              width={2018}
+              height={4090}
+              className="hidden translate-y-12 dark:block"
+            />
+          </div>
+        </div>
+
+        <div
+          style={{ animationDelay: "0.75s", animationFillMode: "forwards" }}
+          className="animate-fade-up col-span-1 overflow-hidden rounded-sm bg-accent/25 p-4 opacity-0 dark:border-t md:p-8"
+        >
+          <FeaturesHeading
+            title="Filter Transactions"
+            description="Lorem ipsum dolor sit aemt consectur lorem ipsum dolor silasd ndsamet consect."
           />
+
+          <div className="flex h-full min-w-[600px] translate-x-[8%] translate-y-6 rounded-md border bg-background p-4">
+            <div className="flex gap-4 pb-12">
+              <Button
+                variant="outline"
+                role="combobox"
+                className={cn(
+                  "flex max-h-10 cursor-default justify-start gap-2 border-brand text-xs text-brand hover:bg-inherit hover:text-brand",
+                )}
+              >
+                <Shapes className="h-3 w-3 opacity-50" />
+                Income
+                <ChevronDown className="ml-auto h-3 w-3 shrink-0 opacity-50" />
+              </Button>
+
+              <Button
+                variant="outline"
+                role="combobox"
+                className={cn(
+                  "flex max-h-10 cursor-default justify-start gap-2 text-xs hover:bg-inherit",
+                )}
+              >
+                <LayoutGrid className="h-3 w-3 opacity-50" />
+                Category
+                <ChevronDown className="ml-auto h-3 w-3 shrink-0 opacity-50" />
+              </Button>
+
+              <Button
+                variant="outline"
+                role="combobox"
+                className={cn(
+                  "flex max-h-10  shrink-0 cursor-default justify-start gap-2 border-brand text-xs  text-brand hover:bg-inherit hover:text-brand",
+                )}
+              >
+                <CalendarIcon className="h-3 w-3 opacity-50" />
+                Dec 22 — Dec 22
+                <ChevronDown className="ml-auto h-3 w-3 shrink-0 opacity-50" />
+              </Button>
+
+              <Button
+                variant="ghost"
+                role="combobox"
+                className={cn(
+                  "flex max-h-10  shrink-0 cursor-default justify-start gap-2 text-xs hover:bg-inherit",
+                )}
+              >
+                <X className="h-3 w-3 opacity-50" />
+                Reset
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- finance-tracker/app/(marketing)/page.tsx
+++ finance-tracker/app/(marketing)/page.tsx
@@ -1,7 +1,5 @@
 import { getCurrentUser } from "@/actions/get-current-user";
 
-import { Benefits } from "./_components/benefits";
-import { Features } from "./_components/features";
 import { FinalCallToAction } from "./_components/final-call-to-action";
 import { Footer } from "./_components/footer";
 import { Hero } from "./_components/hero";
@@ -11,8 +9,6 @@ const HomePage = async () => {
   return (
     <div className="flex flex-col items-center justify-center gap-48">
       <Hero isAuthenticated={!!user} />
-      <Benefits />
-      <Features />
       <FinalCallToAction isAuthenticated={!!user} />
       <Footer />
     </div>
